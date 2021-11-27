95377e69ce6a30a4a971d06dba4513dcc5ae4e24
Lukas <lucasfasilva27@gmail.com>
2021-11-27 16:57:45 -0300

    update

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,63 +5,63 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
-class PostsController extends Controller
-{
-    public function index()
+    class PostsController extends Controller
     {
-        //dd(Post::All());
-        $posts = (Post::All());
+        public function index()
+        {
+            //dd(Post::All());
+            $posts = (Post::All());
 
-        return view('/posts.index', ['posts' => $posts]);
+            return view('/posts.index', ['posts' => $posts]);
 
+        }
+
+        public function create()
+        {
+            return view('posts.create');
+        }
+
+        public function store()
+        {
+            request()->validate([
+                'title' => 'required',
+                'content' => 'required'
+            ]);
+
+            Post::create([
+                'title' => request('title'),
+                'content' => request('content')
+            ]);
+
+            return redirect('/posts');
+        }
+
+        public function edit(Post $post)
+        {
+            return view('posts.edit', ['post' => $post ]); 
+        }
+
+        public function update(Post $post)
+        {
+            //dd(request()->all());
+
+            request()->validate([
+                'title' => 'required',
+                'content' => 'required'
+            ]);
+            $post->update([
+                'title' => request('title'),
+
+                'content' => request('content')
+            ]);
+
+            return redirect('/posts');
+        }
+
+        public function destroy(Post $post)
+        {
+            $post->delete();
+
+            return redirect('/posts');
+        }
     }
-
-    public function create()
-    {
-        return view('posts.create');
-    }
-
-    public function store()
-    {
-        request()->validate([
-            'title' => 'required',
-            'content' => 'required'
-        ]);
-
-        Post::create([
-            'title' => request('title'),
-            'content' => request('content')
-        ]);
-
-        return redirect('/posts');
-    }
-
-    public function edit(Post $post)
-    {
-        return view('posts.edit', ['post' => $post ]); 
-    }
-
-    public function update(Post $post)
-    {
-        //dd(request()->all());
-
-        request()->validate([
-            'title' => 'required',
-            'content' => 'required'
-        ]);
-        $post->update([
-            'title' => request('title'),
-
-            'content' => request('content')
-        ]);
-
-        return redirect('/posts');
-    }
-
-    public function destroy(Post $post)
-    {
-        $post->delete();
-
-        return redirect('/posts');
-    }
-}
